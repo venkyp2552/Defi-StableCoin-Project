5072c3c06e10530610eaad60a8da6f08d148c35a
root <root@DESKTOP-1HVSU4I>
2024-12-20 17:58:31 +0530

    DSC Engine Code Started

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -30,10 +30,68 @@
  * -Exogenous Colletaral
  * -Dollar Pegged
  * -Algorithimically Stable
+ * @Note Our DSC system is always "overcolletaralized",At any point of time, 
+ * all collateral <= the $ backed value of DSC
  * It is Similar to DAI if DAIhad no goverence and no fees and was only backed by WETH & WBTC
+ * 
  * @notice This contract is a core of DSC System.it handles all the logics for minting and reediming DSC
  * as well as depostiting and withdrawing collatoral. 
+ * 
  * This Contract is VERY loosely based on the MarkerDAO DSS(DAI) system.
  */
 pragma solidity ^0.8.18;
 
+
+contract DSCEngine{
+    /////////////////////// 
+    /////// Errors ///////
+    /////////////////////
+    error DSCEngine__NeedMoreThanZer();
+
+    /////////////////////// 
+    /// State Variables ////
+    /////////////////////
+
+    mapping(address token => address priceFeed) private s_priceFeeds;
+
+    /////////////////////// 
+    /////// Modifiers ////
+    /////////////////////
+
+    modifier moreThanZero(uint256 amount) {
+        //Colletral Amount Should Be more than Zer
+        if(amount == 0){
+            revert DSCEngine__NeedMoreThanZer();
+        }
+        _;
+    }
+
+    modifier isAllowedToken(address collateralTokenAddr) {
+        //we will not allow every assets (Every tokens address to mint the DSC except WETH,WBTC)
+        _;
+    }
+
+    constructor(){}
+
+    function depositeCollateralAndMindDsc() external{}
+
+    function depositeCollateral(
+        address tokenCollateralAddress,
+        uint256 collateralAmount
+    ) moreThanZero(collateralAmount) external{
+
+    }
+
+    function reedemCollateralForDsc() external{}
+
+    function reedemCollateral() external{}
+    
+    function mintDsc() external {}
+
+    function burnDsc()external {}
+
+    function liquidate() external{}
+
+    function getHelathFactor() external view {}
+
+}
